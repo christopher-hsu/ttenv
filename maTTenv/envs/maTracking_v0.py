96f6d403f287aaca227f9d1034fd48012c903588
Christopher Hsu <christoherhsu8@gmail.com>
2020-01-29 19:50:42 -0500

    metadata-v0, stable

--- maTTenv/envs/maTracking_v0.py
+++ maTTenv/envs/maTracking_v0.py
@@ -3,10 +3,10 @@ import numpy as np
 from numpy import linalg as LA
 from gym import spaces, logger
 from maTTenv.maps import map_utils
-import maTTenv.env_utils as util 
+import maTTenv.utils as util 
 from maTTenv.agent_models import *
 from maTTenv.belief_tracker import KFbelief
-from maTTenv.metadata import *
+from maTTenv.metadata import METADATA_v0 as METADATA
 from maTTenv.envs.maTracking_Base import maTrackingBase
 
 """
@@ -31,9 +31,12 @@ maTrackingEnv0 : Static Target model + noise - No Velocity Estimate
 
 class maTrackingEnv0(maTrackingBase):
 
-    def __init__(self, hyp, q_true = 0.01, q = 0.01, is_training=True, known_noise=True):
-        super().__init__(hyp, agent_dim=3, target_dim=2, q_true=q_true, q=q)
+    def __init__(self, num_agents=2, num_targets=1, map_name='empty', is_training=True, known_noise=True, **kwargs):
+        super().__init__(num_agents=num_agents, num_targets=num_targets, map_name=map_name)
        
+        self.id = 'maTracking-v0'
+        self.agent_dim = 3
+        self.target_dim = 2
         self.limit['global'] = np.array([[0.0,-np.pi]*(self.num_targets+self.num_agents),
                                 [600.0,np.pi]*(self.num_targets+self.num_agents)])
         self.global_space = spaces.Box(self.limit['global'][0], self.limit['global'][1], dtype=np.float32)
@@ -47,25 +50,25 @@ class maTrackingEnv0(maTrackingBase):
     def setup_agents(self):
         self.agents = [AgentSE2(agent_id = 'agent-' + str(i), 
                         dim=self.agent_dim, sampling_period=self.sampling_period, limit=self.limit['agent'], 
-                        collision_func=lambda x: map_utils.is_collision(self.MAP, x, MARGIN2WALL), 
-                        margin=MARGIN)
+                        collision_func=lambda x: map_utils.is_collision(self.MAP, x))
                         for i in range(self.num_agents)]
 
     def setup_targets(self):
         self.targets = [AgentDoubleInt2D(agent_id = 'target-' + str(i),
                         dim=self.target_dim, sampling_period=self.sampling_period, limit=self.limit['target'],
-                        collision_func=lambda x: map_utils.is_collision(self.MAP, x, MARGIN2WALL),
-                        margin=MARGIN, A=self.targetA, W=self.target_true_noise_sd) 
+                        collision_func=lambda x: map_utils.is_collision(self.MAP, x),
+                        A=self.targetA, W=self.target_true_noise_sd) 
                         for i in range(self.num_targets)]
 
     def setup_belief_targets(self):
         self.belief_targets = [KFbelief(agent_id = 'agent-' + str(i),
                         dim=self.target_dim, limit=self.limit['target'], A=self.targetA,
                         W=self.target_noise_cov, obs_noise_func=self.observation_noise, 
-                        collision_func=lambda x: map_utils.is_collision(self.MAP, x, MARGIN2WALL))
+                        collision_func=lambda x: map_utils.is_collision(self.MAP, x))
                         for i in range(self.num_targets)]
 
-    def reset(self,init_random=True):
+    def reset(self, init_random=True):
+    # def reset(self, **kwargs):
         """
         Agents are given random positions in the map, targets are given random positions near a random agent.
         Return a RL state dict with agent ids (keys) and target state info
@@ -74,6 +77,8 @@ class maTrackingEnv0(maTrackingBase):
         locations = []
         global_state = {}
         full_state = {}
+        # init_pose = self.get_init_pose(**kwargs)
+
         if init_random:
             agents_init = np.zeros((self.num_agents,2))
             # initialize agent locations randomly in map
@@ -81,7 +86,7 @@ class maTrackingEnv0(maTrackingBase):
                 isvalid = False 
                 while(not isvalid):
                     a_init = np.random.random((2,))*(self.MAP.mapmax-self.MAP.mapmin) + self.MAP.mapmin     
-                    isvalid = not(map_utils.is_collision(self.MAP, a_init, MARGIN2WALL))
+                    isvalid = not(map_utils.is_collision(self.MAP, a_init))
                 agents_init[ii,:] = a_init
                 self.agents[ii].reset([a_init[0], a_init[1], np.random.random()*2*np.pi-np.pi])
                 obs_state[self.agents[ii].agent_id] = []
@@ -90,14 +95,14 @@ class maTrackingEnv0(maTrackingBase):
                 isvalid = False
                 while(not isvalid):            
                     rand_ang = np.random.rand()*2*np.pi - np.pi 
-                    t_r = INIT_DISTANCE
+                    t_r = METADATA['init_distance']
                     rand_a = np.random.randint(self.num_agents)
                     rand_a_init = agents_init[rand_a,:]
                     t_init = np.array([t_r*np.cos(rand_ang), t_r*np.sin(rand_ang)]) + rand_a_init
-                    if (np.sqrt(np.sum((t_init - rand_a_init)**2)) < MARGIN):
+                    if (np.sqrt(np.sum((t_init - rand_a_init)**2)) < METADATA['margin']):
                         isvalid = False
                     else:
-                        isvalid = not(map_utils.is_collision(self.MAP, t_init, MARGIN2WALL))
+                        isvalid = not(map_utils.is_collision(self.MAP, t_init))
                 self.belief_targets[jj].reset(init_state=t_init + 10*(np.random.rand(2)-0.5),
                                 init_cov=self.target_init_cov)
                 self.targets[jj].reset(t_init)
@@ -135,7 +140,7 @@ class maTrackingEnv0(maTrackingBase):
 
             action_val = self.action_map[action_dict[agent_id]]
             boundary_penalty = self.agents[ii].update(action_val, [t.state[:2] for t in self.targets])
-            obstacles_pt = map_utils.get_cloest_obstacle(self.MAP, self.agents[ii].state)
+            obstacles_pt = map_utils.get_closest_obstacle(self.MAP, self.agents[ii].state)
             locations.append(self.agents[ii].state[:2])
             
             observed = []
@@ -145,10 +150,10 @@ class maTrackingEnv0(maTrackingBase):
                 observed.append(obs[0])
                 # Update the belief of the agent on the target using KF
                 self.belief_targets[jj].update(obs[0], obs[1], self.agents[ii].state)
-            reward, done, test_reward = self.get_reward(obstacles_pt, observed, self.is_training)
+            reward, done, mean_nlogdetcov = self.get_reward(obstacles_pt, observed, self.is_training)
             reward_dict[agent_id] = reward
             done_dict[agent_id] = done
-            info_dict[agent_id] = test_reward
+            info_dict[agent_id] = mean_nlogdetcov
 
             if obstacles_pt is None:
                 obstacles_pt = (self.sensor_r, np.pi)
