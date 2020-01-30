75c1dafe9322e445a1e9bbe1eb5327beb8e0266f
Christopher Hsu <christoherhsu8@gmail.com>
2020-01-29 19:51:54 -0500

    stable gym use with maTTenv

--- run_ma_example.py
+++ run_ma_example.py
@@ -3,7 +3,7 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--env', help='environment ID', type=str, default='maTracking-v1')
+parser.add_argument('--env', help='environment ID', type=str, default='maTracking-v0')
 parser.add_argument('--render', help='whether to render', type=int, default=0)
 parser.add_argument('--record', help='whether to record', type=int, default=0)
 parser.add_argument('--ros', help='whether to use ROS', type=int, default=0)
@@ -19,19 +19,26 @@ def main():
                     render=args.render,
                     record=args.record,
                     ros=args.ros,
-                    map_name=args.map,
                     directory=args.log_dir,
+                    map_name=args.map,
                     num_agents=args.nb_agents,
                     num_targets=args.nb_targets,
                     is_training=False,
                     )
     nlogdetcov = []
+    action_dict = {}
+
     obs, done = env.reset(), False
     while(not done):
         if args.render:
             env.render()
-        obs, rew, done, info = env.step(env.action_space.sample())
-        nlogdetcov.append(info['mean_nlogdetcov'])
+
+        for agent_id, a_obs in obs.items():
+            action_dict[agent_id] = env.action_space.sample()
+
+        obs, rew, done, info = env.step(action_dict)
+        done = done['__all__']
+        # nlogdetcov.append(info['mean_nlogdetcov'])
 
     print("Sum of negative logdet of the target belief covariances : %.2f"%np.sum(nlogdetcov))
 
