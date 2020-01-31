67002b7a2c4ac06c1842ad2e5778279c7f3c4f9b
Christopher Hsu <christoherhsu8@gmail.com>
2020-01-31 10:46:20 -0500

    rm old funcs

--- maTTenv/utils.py
+++ maTTenv/utils.py
@@ -13,12 +13,6 @@ def vectorized_wrap_around(x):
     x[x<-np.pi] += 2*np.pi
     return x
 
-def relative_measure(x_target, x_main):
-    diff = x_target[:2] - x_main[:2]
-    r = np.sqrt(np.sum(diff**2))
-    alpha = wrap_around(np.arctan2(diff[1],diff[0]) - x_main[2])
-    return r, alpha, diff
-
 def global_relative_measure(x_target, x_main):
     diff = x_target - x_main
     r = np.sqrt(np.sum(diff**2, axis=1))
@@ -26,11 +20,6 @@ def global_relative_measure(x_target, x_main):
     #Rearrange to [r,alpha]*(nb_agents,nb_targets)
     global_state = np.ndarray.flatten(np.vstack((r,alpha)).T)
     return global_state
-
-def coord_change2b(vec, ang):
-    assert(len(vec) == 2)
-    # R^T * v
-    return np.matmul([[np.cos(ang), np.sin(ang)], [-np.sin(ang), np.cos(ang)]], vec)
 
 def cartesian2polar(xy):
     r = np.sqrt(np.sum(xy**2))
