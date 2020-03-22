3e45343afd18d975770741ad57d3659452b48975
Christopher Hsu <christoherhsu8@gmail.com>
2020-03-21 21:31:04 -0400

    agent slower than target

--- maTTenv/metadata.py
+++ maTTenv/metadata.py
@@ -30,14 +30,14 @@ METADATA_v1={   # Beliefs are initialized near target
         'sensor_b_sd': 0.01, # sensor bearing noise.
         'target_init_cov': 30.0, # initial target diagonal Covariance.
         'target_init_vel': 0.0, # target's initial velocity.
-        'target_vel_limit': 2.0, # velocity limit of targets.
+        'target_vel_limit': 3.0, # velocity limit of targets.
         'init_distance_min': 5.0, # the minimum distance btw targets and the agent.
         'init_distance_max': 15.0, # the maximum distance btw targets and the agent.
         'init_belief_distance_min': 1.0, # the minimum distance btw belief and the target.
         'init_belief_distance_max': 7.5, # the maximum distance btw belief and the target.
         'margin': 1.0, # a marginal distance btw targets and the agent.
         'margin2wall': 0.5, # a marginal distance from a wall.
-        'action_v': [3, 2, 1, 0], # action primitives - linear velocities.
+        'action_v': [2, 1.33, 0.67, 0], # action primitives - linear velocities.
         'action_w': [np.pi/2, 0, -np.pi/2], # action primitives - angular velocities.
         'const_q': 0.001, # target noise constant in beliefs.
         'const_q_true': 0.01, # target noise constant of actual targets.
